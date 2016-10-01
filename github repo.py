f8b1cb4c524c1f3af219fdb4f0fef221094c1cca
Priyanshu <priyanshugautam1997@gmail.com>
2016-10-02 01:49:16 +0530

    Update github repo.py

--- github repo.py
+++ github repo.py
@@ -7,14 +7,15 @@ response=urllib.request.urlopen('https://github.com/'+x+'?tab=repositories')
 soup=BeautifulSoup(response,'html.parser')
 a=soup.find('div',{'class':'js-repo-list'})
 list_tag=a.findAll('div',{'class':'public source'})
-print("These are the repositories found.Type the exact name of repository u want to download") 
-print('\n')
+
     
 for tag in list_tag:
     tags=tag.find('a')
     print(tags.string.strip())
     print('\n')
     
+print("These are the repositories found.Type the exact name of repository u want to download") 
+print('\n')    
 y=input()
 
 for tag in list_tag:
@@ -24,9 +25,7 @@ for tag in list_tag:
 
 if(tags.string.strip()==y):
     urllib.request.urlretrieve('https://github.com/'+x.strip()+'/'+tags.string.strip()+'/archive/master.zip','repository.zip')
-    print('The repository is successfully downloaded to your current default directory')
-    x=input()
+    z=input('The repository is successfully downloaded to your current default directory')
 else:
-    print("no such repository found")
-    x=input()
+    p=input("no such repository found")
     
