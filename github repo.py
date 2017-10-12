8d2c2290e27e3dec9b879d94ca9e038e418b4074
Rohan Balot <rohanbalot@gmail.com>
2017-10-13 04:44:57 +0530

    Fixed Pep8

--- github repo.py
+++ github repo.py
@@ -1,32 +1,40 @@
-import urllib.request #requesting response from html
-import shutil # offers a number of high-level operations like copying and removal of files
-import os
-from bs4 import BeautifulSoup # used for scraping data from GitHub
-x=input("type your github handle")
-response=urllib.request.urlopen('https://github.com/'+x+'?tab=repositories')
-soup=BeautifulSoup(response,'html.parser')
-a=soup.find('div',{'class':'js-repo-list'})
-list_tag=a.findAll('div',{'class':'public source'})
+# Requesting response from html
+import urllib.request
+# used for scraping data from GitHub
+from bs4 import BeautifulSoup
+x = input("type your github handle")
+response = urllib.request.urlopen(
+    'https://github.com/' + x + '?tab=repositories'
+)
+soup = BeautifulSoup(response, 'html.parser')
+a = soup.find('div', {'class': 'js-repo-list'})
+list_tag = a.findAll('div', {'class': 'public source'})
 
-# opening tags    
+# opening tags
 for tag in list_tag:
-    tags=tag.find('a')
+    tags = tag.find('a')
     print(tags.string.strip())
     print('\n')
-    
-print("These are the repositories found.Type the exact name of repository u want to download") 
-print('\n')    
-y=input()
+
+print("These are the repositories found. \
+    Type the exact name of repository u want to download")
+print('\n')
+y = input()
 
 for tag in list_tag:
-    tags=tag.find('a')
-    if(tags.string.strip()==y):
+    tags = tag.find('a')
+    if(tags.string.strip() == y):
         break
 
-#dowloading the repo.
-if(tags.string.strip()==y):
-    urllib.request.urlretrieve('https://github.com/'+x.strip()+'/'+tags.string.strip()+'/archive/master.zip','repository.zip')
-    z=input('The repository is successfully downloaded to your current default directory')
+# dowloading the repo.
+if(tags.string.strip() == y):
+    urllib.request.urlretrieve(
+        'https://github.com/%s/%s/archive/master.zip' % (
+            x.strip(), tags.string.strip()
+        ),
+        'repository.zip'
+    )
+    z = input('The repos2itory is successfully downloaded\
+     to your current default directory')
 else:
-    p=input("no such repository found")
-    
+    p = input("no such repository found")
